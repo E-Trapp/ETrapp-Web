82337ef77be282fb601f08738f728fd3f2ba7648
tankintat <tankintat@gmail.com>
2018-01-08 20:47:54 +0100

    Add getAll categories functionality

--- src/main/java/cat/udl/etrapp/server/api/endpoints/CategoriesEndpoint.java
+++ src/main/java/cat/udl/etrapp/server/api/endpoints/CategoriesEndpoint.java
@@ -1,20 +1,42 @@
 package cat.udl.etrapp.server.api.endpoints;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
+import cat.udl.etrapp.server.daos.CategoriesDAO;
+import cat.udl.etrapp.server.models.Category;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/categories")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CategoriesEndpoint {
     // The Java method will process HTTP GET requests
-    @GET
+    /*@GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
     @Path("../test")
     public String getClichedMessage() {
         // Return some cliched textual content
         return "Hello World";
+    }*/
+
+    @GET
+    public Response getAllCategories() {
+        /*String[] a = new String[3];
+        a[0]="1";
+        a[1]="1";
+        a[2]="1";
+
+        return Response.ok(a).build();*/
+        final List<Category> categories = CategoriesDAO.getInstance().getCategories();
+        if (categories.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            return Response.ok(categories).build();
+        }
     }
 
 }
--- src/main/java/cat/udl/etrapp/server/daos/CategoriesDAO.java
+++ src/main/java/cat/udl/etrapp/server/daos/CategoriesDAO.java
@@ -1,5 +1,16 @@
 package cat.udl.etrapp.server.daos;
 
+import cat.udl.etrapp.server.db.DBManager;
+import cat.udl.etrapp.server.models.Category;
+import cat.udl.etrapp.server.models.Event;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoriesDAO {
 
     // Get category and its descendents
@@ -9,5 +20,51 @@ public class CategoriesDAO {
 //    SELECT c.id, c.parent_id, p.depth + 1 AS depth, (p.path || '->' || c.id::TEXT) FROM nodes_cte AS p, categories AS c WHERE c.parent_id = p.id
 //)
 //    SELECT * FROM nodes_cte AS n;
+
+    private static CategoriesDAO instance;
+
+    private CategoriesDAO() {
+    }
+
+    public static synchronized CategoriesDAO getInstance() {
+        if (instance == null) instance = new CategoriesDAO();
+        return instance;
+    }
+
+    public List<Category> getCategories() {
+        final String SQLQuery;
+
+        SQLQuery = "SELECT * FROM categories ORDER BY name ASC";
+
+        final List<Category> categories = new ArrayList<>();
+
+        try (Connection connection = DBManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SQLQuery)) {
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    categories.add(eventFromResultSet(resultSet));
+                }
+            } catch (SQLException e) {
+                System.err.println("Error in SQL: getEvents");
+                System.err.println(e.getMessage());
+            }
+        } catch (SQLException e) {
+            System.err.println("Error in SQL: getEvents()");
+            System.err.println(e.getMessage());
+        }
+
+        return categories;
+    }
+
+    private Category eventFromResultSet(ResultSet resultSet) throws SQLException {
+        final Category category = new Category();
+        category.setId(resultSet.getLong("id"));
+        category.setName(resultSet.getString("name"));
+        category.setParentId(resultSet.getLong("parent_id"));
+
+        return category;
+    }
+
 
 }
